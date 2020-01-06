eacada5b418139e2b9f104fb76f34c9425865165
yanghaodong6295 <yanghaodong6295@ipalfish.com>
2020-01-06 15:22:01 +0800

    取消cpu限制，增加超时限制

--- core/detect_functions.py
+++ core/detect_functions.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import numpy as np
 import json
+import time as Time
 
 import config
 from core.utils.datasets import get_labels
@@ -52,6 +53,57 @@ def get_emotion_stream_cut(video_path, detector, frame_interval_ms, start_ms, en
             continue
         time = int(video_capture.get(cv2.CAP_PROP_POS_MSEC))
         prediction, _ = detector.detect_biggest(frame)
+        if prediction is None:
+            start_frame_no += interval_frame_num
+            continue
+        # 计算最最大表情值所占百分比，再乘以1000，转为整数，再除以1000，变为float相当于保留三位小数
+        rate = int(np.max(prediction) / np.sum(prediction) * 1000) / 1000.0
+        emotion_text = get_labels('fer2013')[np.argmax(prediction)]
+        frame_emotion = FrameEmotion(time, emotion_text, rate)
+        # 转换为便于转成json的字典格式
+        emotion_stream.append(frame_emotion.__dict__)
+        start_frame_no += interval_frame_num
+    video_capture.release()
+    return emotion_stream
+
+
+# 只分析指定范围内的视频，end_ms小于零代表分析到末尾，增添超时控制
+def get_emotion_stream_cut_with_timeout(video_path, detector, frame_interval_ms, start_ms, end_ms, time_limit_second):
+    video_capture = cv2.VideoCapture(video_path)
+    try:
+        video_capture.isOpened()
+    except Exception as ex:
+        raise ex
+    fps = video_capture.get(cv2.CAP_PROP_FPS)
+    frame_count = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+    start_frame_no = ms2frame(fps, start_ms) + 1  # 加一防止为0的情况
+    end_frame_no = ms2frame(fps, end_ms)
+
+    if end_ms < 0 or end_frame_no > frame_count:
+        end_frame_no = frame_count
+    if start_frame_no < 1:
+        start_frame_no = 1
+    if start_frame_no > end_frame_no:
+        return []
+
+    interval_frame_num = ms2frame(fps, frame_interval_ms)
+    emotion_stream = []
+    if interval_frame_num < 1:
+        interval_frame_num = 1  # 防止帧间隔为0的情况
+
+    start_time = Time.time()
+    while start_frame_no <= end_frame_no:
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame_no)
+        frame = video_capture.read()[1]
+        if frame is None or np.size(frame) is 0:
+            start_frame_no += interval_frame_num
+            continue
+        time = int(video_capture.get(cv2.CAP_PROP_POS_MSEC))
+        prediction, _ = detector.detect_biggest(frame)
+        now_time = Time.time()
+        if now_time - start_time > time_limit_second:
+            raise APIException(500, config.TIMEOUT)
+
         if prediction is None:
             start_frame_no += interval_frame_num
             continue
@@ -200,3 +252,10 @@ def get_emotion_stream_cut_json(video_path, detector, frame_interval_ms, start_m
     emotion_stream_json = json.dumps(emotion_stream)
     return emotion_stream_json
 
+
+# 只分析指定范围内的视频，返回json
+def get_emotion_stream_cut_json_with_timeout(video_path, detector, frame_interval_ms, start_ms, end_ms, timeout_second):
+    emotion_stream = get_emotion_stream_cut_with_timeout(video_path, detector, frame_interval_ms, start_ms, end_ms,
+                                                         timeout_second)
+    emotion_stream_json = json.dumps(emotion_stream)
+    return emotion_stream_json
